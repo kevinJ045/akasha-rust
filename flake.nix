aaa935c1870ae3d68da8878289226e46d139f8f6
makano <makanobush@gmail.com>
2025-04-21 00:15:53 +0300

    ...idk

--- flake.nix
+++ flake.nix
@@ -13,10 +13,7 @@
           mesa
           libGL
           libGLU
-          cargo
-          rustc
           rustup
-          rustfmt
           libxkbcommon
           wayland
           xorg.libX11
@@ -24,8 +21,36 @@
           wayland
           openssl
           pkg-config
+          pkgsCross.mingwW64.stdenv.cc
+          xorg.libXcursor
+          xorg.libXrandr
+          readline
+          xorg.libXi
+          ncurses
+          glibc
         ];
       in {
+        packages.x86_64-linux = with nixpkgs.pkgs; rec {
+          akasha = stdenv.mkDerivation rec {
+            pname = "akasha";
+            version = "1.0.0";
+
+            src = fetchFromGitHub {
+              owner = "kevinj045";
+              repo = "akasha-rust";
+              rev = "v1.0.0"; # Tag that corresponds to the release
+              sha256 = "0d197ae735ad26c6446265b0bc3168fde4f1fac5890e1f8d3b137b4b53fc22cd"; # You can get this from `nix-prefetch-url` or `nix-prefetch-git`
+            };
+
+            buildInputs = libraries;
+
+            meta = with lib; {
+              description = "Akasha.cv desktop app";
+              platforms = platforms.linux;
+            };
+          };
+        };
+
         devShells.default =
           pkgs.mkShell {
             buildInputs = libraries;
@@ -33,6 +58,18 @@
             shellHook = ''
               export PKG_CONFIG_PATH="${openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              
+              if [ ! -f "$HOME/.cargo/env" ]; then
+                rustup-init -y
+              fi
+              source "$HOME/.cargo/env"
+
+              # Set default toolchain if not already set
+              rustup show | grep -q "stable" || rustup default stable
+
+              # Ensure windows target is installed
+              rustup target add x86_64-pc-windows-gnu
+
             '';
           };
       });
